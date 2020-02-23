668ffb465fa703b464ee8181bfa8a1218079652c
Xavier Mod <xaviermod@xaviers-mbp.home>
2020-02-23 22:13:17 +0000

    new update

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
       spaceId: process.env.CONTENTFUL_ID,
       // Learn about environment variables: https://gatsby.dev/env-vars
       accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
     },
   },
   `gatsby-plugin-styled-components`,
